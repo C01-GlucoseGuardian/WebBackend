3ecfb13b93722087917f994e8cf0cbf98b571b48
AmineSr97 <122969089+AmineSr97@users.noreply.github.com>
2023-01-18 13:11:29 +0100

    Add files via upload

---
+++ WebBackend/src/main/java/com/GlucoseGuardian/WebBackend/WebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GlucoseGuardian.WebBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBackendApplication.class, args);
+	}
+
+}
---
+++ WebBackend/src/test/java/com/GlucoseGuardian/WebBackend/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GlucoseGuardian.WebBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

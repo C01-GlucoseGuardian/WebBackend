3f704bc7eefac9d80919650f3ebea05e8040c41c
vito <vitopiegari01@gmail.com>
2023-01-18 21:20:41 +0100

    [checkstyle] Please work

--- src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/Farmaco.java
+++ src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/Farmaco.java
@@ -4,62 +4,68 @@ import jakarta.persistence.*;
 
 @Entity
 public class Farmaco {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(columnDefinition = "UNSIGNED INT")
-    private int id;
 
-    @Column(columnDefinition = "VARCHAR(50)")
-    private String nomeFarmaco;
-    @Column(columnDefinition = "VARCHAR(255)")
-    private String principioAttivo;
-    @Column(columnDefinition = "VARCHAR(255)")
-    private String confezione;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(columnDefinition = "UNSIGNED INT")
+  private int id;
+
+  @Column(columnDefinition = "VARCHAR(50)")
+  private String nomeFarmaco;
+  @Column(columnDefinition = "VARCHAR(255)")
+  private String principioAttivo;
+  @Column(columnDefinition = "VARCHAR(255)")
+  private String confezione;
 
 
-    public Farmaco() {
-    }
+  public Farmaco() {
+  }
 
-    public Farmaco(String nomeFarmaco, String principioAttivo, String confezione) {
-        this.nomeFarmaco = nomeFarmaco;
-        this.principioAttivo = principioAttivo;
-        this.confezione = confezione;
-    }
+  public Farmaco(String nomeFarmaco, String principioAttivo, String confezione) {
+    this.nomeFarmaco = nomeFarmaco;
+    this.principioAttivo = principioAttivo;
+    this.confezione = confezione;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getNomeFarmaco() {
-        return nomeFarmaco;
-    }
+  public String getNomeFarmaco() {
+    return nomeFarmaco;
+  }
 
-    public void setNomeFarmaco(String nomeFarmaco) {
-        this.nomeFarmaco = nomeFarmaco;
-    }
+  public void setNomeFarmaco(String nomeFarmaco) {
+    this.nomeFarmaco = nomeFarmaco;
+  }
 
-    public String getPrincipioAttivo() {
-        return principioAttivo;
-    }
+  public String getPrincipioAttivo() {
+    return principioAttivo;
+  }
 
-    public void setPrincipioAttivo(String principioAttivo) {
-        this.principioAttivo = principioAttivo;
-    }
+  public void setPrincipioAttivo(String principioAttivo) {
+    this.principioAttivo = principioAttivo;
+  }
 
-    public String getConfezione() {
-        return confezione;
-    }
+  public String getConfezione() {
+    return confezione;
+  }
 
-    public void setConfezione(String confezione) {
-        this.confezione = confezione;
-    }
+  public void setConfezione(String confezione) {
+    this.confezione = confezione;
+  }
 
-    @Override
-    public String toString() {
-        return "Farmaco{" + "id=" + id + ", nomeFarmaco='" + nomeFarmaco + '\'' + ", principioAttivo='" + principioAttivo + '\'' + ", confezione='" + confezione + '\'' + '}';
-    }
+  @Override
+  public String toString() {
+    return "Farmaco{" +
+        "id=" + id +
+        ", nomeFarmaco='" + nomeFarmaco + '\'' +
+        ", principioAttivo='" + principioAttivo + '\'' +
+        ", confezione='" + confezione + '\'' +
+        '}';
+  }
 }
--- src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/ProfiloTutore.java
+++ src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/ProfiloTutore.java
@@ -9,53 +9,68 @@ import java.util.Date;
 
 @Entity
 public class ProfiloTutore implements Serializable {
-    @Id
-    @Column(columnDefinition = "CHAR(16)")
-    private String codiceFiscale;
 
-    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
-    private String nome;
-    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+  @Id
+  @Column(columnDefinition = "CHAR(16)")
+  private String codiceFiscale;
 
-    private String cognome;
-    @Column(nullable = false)
-    private Date dataNascita;
-    @Column(columnDefinition = "VARCHAR(50)", nullable = false)
-    private String indirizzo;
-    @Column(columnDefinition = "VARCHAR(15)", nullable = false)
-    private String telefono;
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
-    private String email;
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
-    private String password;
-    @Column(columnDefinition = "CHAR(1)", nullable = false)
-    private char sesso;
+  @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+  private String nome;
+  @Column(columnDefinition = "VARCHAR(30)", nullable = false)
 
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
-    private String totpKey;
+  private String cognome;
+  @Column(nullable = false)
+  private Date dataNascita;
+  @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+  private String indirizzo;
+  @Column(columnDefinition = "VARCHAR(15)", nullable = false)
+  private String telefono;
+  @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+  private String email;
+  @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+  private String password;
+  @Column(columnDefinition = "CHAR(1)", nullable = false)
+  private char sesso;
 
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
-    private String relazioneDiParentela;
+  @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+  private String totpKey;
 
-    public ProfiloTutore() {
-    }
+  @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+  private String relazioneDiParentela;
 
-    public ProfiloTutore(String codiceFiscale, String nome, String cognome, Date dataNascita, String indirizzo, String telefono, String email, String password, char sesso, String totpKey, String relazioneDiParentela) {
-        this.codiceFiscale = codiceFiscale;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.dataNascita = dataNascita;
-        this.indirizzo = indirizzo;
-        this.telefono = telefono;
-        this.email = email;
-        this.password = password;
-        this.sesso = sesso;
-        this.totpKey = totpKey;
-        this.relazioneDiParentela = relazioneDiParentela;
-    }
+  public ProfiloTutore() {
+  }
 
-    @Override
-    public String toString() {
-        return "ProfiloTutore{" + "codiceFiscale='" + codiceFiscale + '\'' + ", nome='" + nome + '\'' + ", cognome='" + cognome + '\'' + ", dataNascita=" + dataNascita + ", indirizzo='" + indirizzo + '\'' + ", telefono='" + telefono + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", sesso=" + sesso + ", totpKey='" + totpKey + '\'' + ", relazioneDiParentela='" + relazioneDiParentela + '\'' + '}';
-    }
+  public ProfiloTutore(String codiceFiscale, String nome, String cognome, Date dataNascita,
+      String indirizzo, String telefono, String email, String password, char sesso, String totpKey,
+      String relazioneDiParentela) {
+    this.codiceFiscale = codiceFiscale;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.dataNascita = dataNascita;
+    this.indirizzo = indirizzo;
+    this.telefono = telefono;
+    this.email = email;
+    this.password = password;
+    this.sesso = sesso;
+    this.totpKey = totpKey;
+    this.relazioneDiParentela = relazioneDiParentela;
+  }
+
+  @Override
+  public String toString() {
+    return "ProfiloTutore{" +
+        "codiceFiscale='" + codiceFiscale + '\'' +
+        ", nome='" + nome + '\'' +
+        ", cognome='" + cognome + '\'' +
+        ", dataNascita=" + dataNascita +
+        ", indirizzo='" + indirizzo + '\'' +
+        ", telefono='" + telefono + '\'' +
+        ", email='" + email + '\'' +
+        ", password='" + password + '\'' +
+        ", sesso=" + sesso +
+        ", totpKey='" + totpKey + '\'' +
+        ", relazioneDiParentela='" + relazioneDiParentela + '\'' +
+        '}';
+  }
 }

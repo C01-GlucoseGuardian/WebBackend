e9e5d4473da8399cc8baea5bae0837f466595fda
matteoaldi <matteoaldi1@gmail.com>
2023-01-18 16:50:30 +0100

    Create Entities Dottore e Glicemia

---
+++ src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/Glicemia.java
@@ -0,0 +1,21 @@
+package com.GlucoseGuardian.WebBackend.storage.entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+public class Glicemia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UNSIGNED INT",nullable = false)
+    private int id;
+    @Column(nullable = false)
+    private int livelloGlucosio;
+    @Column(nullable = false)
+    private Date data;
+    @Column(nullable = false)
+    private Time ora;
+
+}

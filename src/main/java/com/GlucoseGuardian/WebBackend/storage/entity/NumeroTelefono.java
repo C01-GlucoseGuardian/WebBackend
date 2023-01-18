b16ff0219c0cecf7162c2a489f68d077e8bd2a7f
handymenny <handymenny@outlook.com>
2023-01-18 22:27:48 +0100

    entity NumeroTelefono: address checkstyle complaints

--- src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/NumeroTelefono.java
+++ src/main/java/com/GlucoseGuardian/WebBackend/storage/entity/NumeroTelefono.java
@@ -4,46 +4,48 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import java.io.Serializable;
 
+/**
+ * Questa classe rappresenta l'entity NumeroTelefono.
+ */
 public class NumeroTelefono implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @Column(length = 15, nullable = false)
-    private String numero;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+  @Column(length = 15, nullable = false)
+  private String numero;
 
-    public NumeroTelefono() {
+  public NumeroTelefono() {
 
-    }
+  }
 
-    public NumeroTelefono(String numero) {
-        this.numero = numero;
-    }
+  public NumeroTelefono(String numero) {
+    this.numero = numero;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getNumero() {
-        return numero;
-    }
+  public String getNumero() {
+    return numero;
+  }
 
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
 
-    @Override
-    public String toString() {
-        return "NumDiTelefono{" +
-                "id=" + id +
-                ", numero='" + numero + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "NumDiTelefono{"
+        + "id=" + id
+        + ", numero='" + numero + '\''
+        + '}';
+  }
 }

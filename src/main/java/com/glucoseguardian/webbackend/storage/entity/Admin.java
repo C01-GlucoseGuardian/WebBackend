193020df0c7770ce6f2e10df32e01b5b6bc83165
handymenny <handymenny@outlook.com>
2023-01-19 16:20:19 +0100

    admin: set nullable to false

--- src/main/java/com/glucoseguardian/webbackend/storage/entity/Admin.java
+++ src/main/java/com/glucoseguardian/webbackend/storage/entity/Admin.java
@@ -34,7 +34,7 @@ public class Admin implements Serializable {
   private String email;
   @Column(nullable = false)
   private String password;
-  @Column(columnDefinition = "CHAR(1)")
+  @Column(columnDefinition = "CHAR(1)", nullable = false)
   private char sesso;
   @Column
   private String totpKey;
@@ -46,7 +46,7 @@ public class Admin implements Serializable {
   private String nomeStruttura;
   @Column(length = 100, nullable = false)
   private String indirizzoStruttura;
-  @Column(columnDefinition = "UNSIGNED INT(1)")
+  @Column(columnDefinition = "UNSIGNED INT(1)", nullable = false)
   private int stato = 0;
   @OneToMany(mappedBy = "adminDestinatario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   private List<Notifica> notifiche;

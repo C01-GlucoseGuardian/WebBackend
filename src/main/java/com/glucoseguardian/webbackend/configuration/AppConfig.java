c2f9e9740dd3918277847657c8ac6a3fcc551c6c
handymenny <handymenny@outlook.com>
2023-01-27 22:45:52 +0100

    add jwtAuth/filter

--- src/main/java/com/glucoseguardian/webbackend/configuration/AppConfig.java
+++ src/main/java/com/glucoseguardian/webbackend/configuration/AppConfig.java
@@ -1,17 +1,33 @@
 package com.glucoseguardian.webbackend.configuration;
 
-
+import com.glucoseguardian.webbackend.storage.dao.UtenteDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * JavaSpring configuration.
  */
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+  private final @NonNull UtenteDao utenteDao = new UtenteDao();
+
+  private final @NonNull JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter();
+
   /**
    * Usa PBKDF2 come raccomandato dal NIST con i valori consigliati da OWASP.
    */
@@ -19,5 +35,38 @@ public class AppConfig {
   public PasswordEncoder encoder() {
     return new Pbkdf2PasswordEncoder("", 16, 600_000,
         SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+  }
+
+  @Bean
+  public @NonNull UserDetailsService userDetailsService() throws UsernameNotFoundException {
+    return username -> utenteDao.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Utente non trovato"));
+  }
+
+  /**
+   * Authentication Provider.
+   */
+  @Bean
+  public @NonNull AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(encoder());
+    return authProvider;
+  }
+
+  /**
+   * Security Policy basata su {@link JwtAuthenticationFilter}.
+   */
+  @Bean
+  public @NonNull SecurityFilterChain securityFilterChain(@NonNull HttpSecurity http)
+      throws Exception {
+    // TODO: Customize security policy
+    http
+        .csrf().disable()
+        .authorizeHttpRequests().anyRequest().authenticated().and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
   }
 }
---
+++ src/main/java/com/glucoseguardian/webbackend/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,77 @@
+package com.glucoseguardian.webbackend.configuration;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import com.glucoseguardian.webbackend.application.service.JwtService;
+import com.glucoseguardian.webbackend.storage.entity.Utente;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Requestfilter basato su token jwt. Utilizza {@link JwtService} per la verifica dei token.
+ */
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private static final String BEARER = "Bearer ";
+
+  private JwtService jwtService = new JwtService();
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+    // Don't check jwt if user is already authenticated
+    if (SecurityContextHolder.getContext().getAuthentication() == null) {
+      // Get jwt from request
+      Optional<String> jwt = getJwtFromRequest(request);
+      jwt.ifPresent(token -> {
+        String email = jwtService.getUsername(token);
+        if (email != null) {
+          try {
+            Utente user = (Utente) this.userDetailsService.loadUserByUsername(email);
+            if (jwtService.isTokenValid(token, user)) {
+              // jwt is valid we can authenticate the user
+              SecurityContextHolder
+                  .getContext()
+                  .setAuthentication(
+                      new UsernamePasswordAuthenticationToken(user, token, user.getAuthorities())
+                  );
+            }
+          } catch (UsernameNotFoundException ignored) {
+            // Do nothing
+          }
+        }
+      });
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  /**
+   * Ritorna il token jwt dall'header Authorization.
+   */
+  private Optional<String> getJwtFromRequest(@NonNull HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION);
+    if (bearerToken == null || !bearerToken.startsWith(BEARER)) {
+      return Optional.empty();
+    }
+    return Optional.of(bearerToken.substring(7));
+  }
+}

904701e93b30210e18fb367209576f3d59eeb8b0
Marco <80520971+IamMarco29@users.noreply.github.com>
2023-01-31 23:47:55 +0100

    Add NotificaServiceStub

---
+++ src/main/java/com/glucoseguardian/webbackend/application/service/notifica/NotificaServiceStub.java
@@ -0,0 +1,40 @@
+package com.glucoseguardian.webbackend.application.service.notifica;
+
+import com.glucoseguardian.webbackend.storage.dto.ListDto;
+import com.glucoseguardian.webbackend.storage.dto.NotificaDto;
+
+/**
+ * This is the stub.
+ */
+public class NotificaServiceStub implements NotificaServiceInterface {
+
+  @Override
+  public NotificaDto findById(Long id) {
+    return new NotificaDto();
+  }
+
+  @Override
+  public boolean send(NotificaDto notificaDto) {
+    return true;
+  }
+
+  @Override
+  public ListDto<NotificaDto> findByPaziente(String codiceFiscale) {
+    return new ListDto<>();
+  }
+
+  @Override
+  public ListDto<NotificaDto> findByDottore(String codiceFiscale) {
+    return new ListDto<>();
+  }
+
+  @Override
+  public ListDto<NotificaDto> findByTutore(String codiceFiscale) {
+    return new ListDto<>();
+  }
+
+  @Override
+  public ListDto<NotificaDto> findByAdmin(String codiceFiscale) {
+    return new ListDto<>();
+  }
+}

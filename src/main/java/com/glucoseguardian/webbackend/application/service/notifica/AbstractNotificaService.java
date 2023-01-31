14157e29ed5a178340666ea6ca073e2d170e1f80
Marco <80520971+IamMarco29@users.noreply.github.com>
2023-01-31 16:02:37 +0100

    Add AbstractNotificaService

---
+++ src/main/java/com/glucoseguardian/webbackend/application/service/notifica/AbstractNotificaService.java
@@ -0,0 +1,10 @@
+package com.glucoseguardian.webbackend.application.service.notifica;
+
+/**
+ * This is an abstact class of NotificaService.
+ */
+public abstract class AbstractNotificaService {
+
+  public abstract NotificaServiceInterface getImplementation();
+}
+

8e0bfde1587df337db4a12862ed46bf924baf59e
Marco <80520971+IamMarco29@users.noreply.github.com>
2023-02-01 16:45:40 +0100

    FarmacoService: Add @PreAuthorize annotations

--- src/main/java/com/glucoseguardian/webbackend/application/service/farmaco/FarmacoServiceInterface.java
+++ src/main/java/com/glucoseguardian/webbackend/application/service/farmaco/FarmacoServiceInterface.java
@@ -4,12 +4,15 @@ import com.glucoseguardian.webbackend.exceptions.UserNotFoundException;
 import com.glucoseguardian.webbackend.storage.dto.FarmacoDto;
 import com.glucoseguardian.webbackend.storage.dto.ListDto;
 import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * This is a FarmacoService Interface.
  */
 public interface FarmacoServiceInterface {
+  @PreAuthorize("hasAuthority('DOTTORE') or hasAuthority('PAZIENTE') or hasAuthority('TUTORE')")
   FarmacoDto findById(Long id) throws UserNotFoundException;
 
+  @PreAuthorize("hasAuthority('DOTTORE') or hasAuthority('PAZIENTE') or hasAuthority('TUTORE')")
   ListDto<FarmacoDto> findFarmaco(String ricerca);
 }

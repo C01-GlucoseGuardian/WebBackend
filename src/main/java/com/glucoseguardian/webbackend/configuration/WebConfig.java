69f8eb803afe8353ba3673b2235533ee8f3fb4af
handymenny <handymenny@outlook.com>
2023-02-11 00:00:34 +0100

    enable cors

---
+++ src/main/java/com/glucoseguardian/webbackend/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.glucoseguardian.webbackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS headers configuration.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+
+}

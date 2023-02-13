609d492417c87d3f3f3a49ad7c06c5b0d7bd8594
matteoaldi <matteoaldi1@gmail.com>
2023-02-13 15:11:15 +0100

    DaoServiceTest

---
+++ src/test/java/com/glucoseguardian/webbackend/integrationtests/restservicedao/AbstractIntegrationDaoTest.java
@@ -0,0 +1,73 @@
+package com.glucoseguardian.webbackend.integrationtests.restservicedao;
+
+import static org.hamcrest.Matchers.anything;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.glucoseguardian.webbackend.autenticazione.service.JwtService;
+import com.glucoseguardian.webbackend.storage.dao.AdminDao;
+import com.glucoseguardian.webbackend.storage.dao.DottoreDao;
+import com.glucoseguardian.webbackend.storage.dao.PazienteDao;
+import com.glucoseguardian.webbackend.storage.dao.TutoreDao;
+import com.glucoseguardian.webbackend.storage.dao.UtenteDao;
+import com.glucoseguardian.webbackend.storage.entity.Utente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+
+@SpringBootTest
+public abstract class AbstractIntegrationDaoTest {
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @MockBean
+  UtenteDao utenteDao;
+  @MockBean
+  PazienteDao pazienteDao;
+  @MockBean
+  TutoreDao tutoreDao;
+  @MockBean
+  DottoreDao dottoreDao;
+  @MockBean
+  AdminDao adminDao;
+  @Autowired(required = false)
+  JwtService jwtService;
+
+  @Autowired
+  private MockMvc mvc;
+
+  /**
+   * Esegue una chiamata MVC attendendo per la risposta nel caso sia asincrona o restituendola se è
+   * sincrona.
+   */
+  protected ResultActions performSync(MockHttpServletRequestBuilder requestBuilder)
+      throws Exception {
+    ResultActions resultActions = mvc.perform(requestBuilder);
+    if (!resultActions.andReturn().getRequest().isAsyncStarted()) {
+      return resultActions;
+    }
+    return mvc.perform(
+        asyncDispatch(resultActions.andExpect(request().asyncResult(anything())).andReturn()));
+  }
+
+  /**
+   * Serializza un oggetto nel formato json.
+   */
+  protected String toJsonString(Object obj) {
+    try {
+      return mapper.writeValueAsString(obj);
+    } catch (JsonProcessingException ex) {
+      ex.printStackTrace();
+      return null;
+    }
+  }
+
+  protected String generateJwtToken(Utente user) {
+    return jwtService.generateToken(user);
+  }
+}
